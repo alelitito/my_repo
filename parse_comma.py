07a0e5851a8872b0a2d6de23f4788855a2c6933e
Aleli A. Tito <aleli.tito@gmail.com>
2016-02-26 13:47:55 +0800

    Added Python script

---
+++ parse_comma.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+f = open('C:/Users/ptito/Desktop/bsageng1b.txt','r')
+
+name_list = []
+
+for i in f:
+   name_list.append(str(i))
+
+i = 0
+while i < len(name_list):
+   #name = (((name_list[i]).split()[1]) + ' ' + ((name_list[i]).split()[2])).replace(',','')
+   name=((name_list[i]).split()[0]).replace(',','')
+   print name
+   i += 1
+
+
+
+#(((name_list[i]).split()[1]) + ((name_list[i]).split()[2]))
